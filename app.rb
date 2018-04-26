28ae71b386a11ca94f32cd2b61c4835e3b287a9d
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-26 18:21:38 -0400

    HTML and CSS changes and experiments

--- app.rb
+++ app.rb
@@ -13,42 +13,81 @@ set :database, {adapter: 'postgresql', database: 'crime_blog'}
 
 enable :sessions
 
+#welcome page
+get '/' do 
+    # if logged_in?
+    #     redirect to '/users/show'
+    # else
+        erb :index
+end
+# end
+
+#homepage
+get '/home' do 
+    @posts = Post.all
+    erb :home
+end
+
+#signup page to create new user
 get '/signup' do
     erb :'/users/signup'
 end
 
-get '/login' do
-    erb :'users/login'
-end
-
-get '/profile' do
-    @user = User.find(session[:id])
-    erb :'/users/show'
-
-get '/logout' do
-    session.clear
-    redirect 'users/login'
-end
-    
-post '/user/login' do 
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user != nil
-        session[:id] = @user.id
-        erb :profile
-    else   
-        redirect 'users/signup'
-    end 
-end
-    
+#create new user
 post '/user/new' do 
     @newuser = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], birthday: params[:birthday])
     session[:id] = @newuser.id
     redirect 'users/show'
 end
 
+#login page
+get '/login' do
+    erb :'users/login'
+end
+
+#process login request 
+post '/user/login' do 
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user != nil
+        session[:id] = @user.id
+        erb :'users/show'
+    else   
+        redirect 'users/signup'
+    end 
+end
+
+#logour process
+get '/logout' do
+    session.clear
+    redirect 'users/login'
+end
+
+#show a user's profile and posts
+get '/profile' do
+    @user = User.find(session[:id])
+    erb :'/users/show'
+
+#user can delete their account
 delete '/user/:id' do 
     User.destroy(params[:id])
     redirect 'users/signup'
 end
 end
-    
+
+#create new post
+get '/posts/new' do
+     erb :'posts/new'
+end
+
+#view all posts
+get '/posts/all_posts' do 
+    erb :'posts/all_posts'
+end
+
+#show a user's posts
+get '/user/posts' do 
+    erb :'/users/show'
+end
+
+
+   
