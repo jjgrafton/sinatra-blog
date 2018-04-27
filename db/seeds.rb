d9ff331e014f9c8c453afcb9f9683a08eeecaf5d
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-26 22:31:05 -0400

    Got navbar and all posts page working, plus aligned header

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ require 'faker'
 
 User.create(first_name: Faker::Hipster.word, last_name: Faker::Hipster.word, email: "hipster@hipster.com", password: "123123", birthday: "1990-01-08" )
 
-Post.create(title: Faker::Hipster.sentence(3), date: "2018-26-04", content: Faker::Hipster.paragraph)
+20.times do |index|
+Post.create(title: Faker::Hipster.sentence(10), date: "2018-26-04", content: Faker::Hipster.paragraph)
+end
